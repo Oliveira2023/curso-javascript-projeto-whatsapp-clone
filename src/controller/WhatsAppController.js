9fd864a1dd7fbdb3b9dfb1495037dc2706ab0e95
Luciano Oliveira <luciano.oliveira96@yahoo.com.br>
2023-07-13 22:50:59 -0300

    updated130723
    botoes de anexar

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -157,7 +157,114 @@ class WhatsAppController {
             let formDAta = new FormData(this.el.formPanelAddContact)
         
         })
+
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item=>{
+            item.on('click', e=>{
+                this.el.home.hide()
+                this.el.main.css({
+                    display:'flex'
+                })
+            })
+        })
+
+        this.el.btnAttach.on('click', e=>{
+        
+            e.stopPropagation()
+            this.el.menuAttach.addClass('open')
+            document.addEventListener('click', this.closeMenuAttach.bind(this))
+
+        })
+
+        this.el.btnAttachPhoto.on('click', e=>{
+            
+            this.el.inputPhoto.click()
+
+        })
+
+        this.el.inputPhoto.on('change', e=>{
+            
+            console.log(this.el.inputPhoto.files)
+            let fotoList = [...this.el.inputPhoto.files]
+            fotoList.forEach(file=>{
+                console.log(file)
+            })
+        })
+
+        this.el.btnAttachCamera.on('click', e=>{
+            
+            this.closeAllMainPanel()
+            this.el.panelCamera.addClass('open')
+            this.el.panelCamera.css({
+                'height': '100%'
+            })
+        })
+
+        this.el.btnClosePanelCamera.on('click',e=>{
+        
+            this.closeAllMainPanel()
+            this.el.panelMessagesContainer.show()
+        
+        })
+
+        this.el.btnTakePicture.on('click', e=>{
+            
+            console.log('take picture')
+
+        })
+
+        this.el.btnAttachDocument.on('click', e=>{
+            
+            this.closeAllMainPanel()
+            this.el.panelDocumentPreview.addClass('open')
+            this.el.panelDocumentPreview.css({
+                'height': '100%'
+            })
+        })
+
+        this.el.btnClosePanelDocumentPreview.on('click',e=>{
+        
+            this.closeAllMainPanel()
+            this.el.panelMessagesContainer.show()
+        })
+
+        this.el.btnSendDocument.on('click', e=>{
+        
+            
+            this.el.inputDocument.click()
+            let fileList = [...this.el.inputDocument.files]
+            fileList.forEach(file=>{
+             console.log(file)
+            })
+        })
+
+
+        this.el.btnAttachContact.on('click', e=>{
+            
+            this.el.modalContacts.show()
+
+        })
+
+        this.el.btnCloseModalContacts.on('click', ()=>{
+            this.el.modalContacts.hide()
+        })
     }
+
+    closeAllMainPanel(){
+
+        this.el.panelMessagesContainer.hide()
+        this.el.panelDocumentPreview.removeClass()
+        this.el.panelCamera.removeClass()
+
+    }
+
+    closeMenuAttach(e){
+
+        removeEventListener('click', this.closeMenuAttach)
+        
+        this.el.menuAttach.removeClass('open')
+        
+    }
+
 
     closeAllLeftPanel(){
     
